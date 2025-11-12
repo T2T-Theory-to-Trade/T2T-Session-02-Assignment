96ba4377b59a1568d3e61d055984c4230f5801d4
ruchiralak <ruchiralakshanm@gmail.com>
2025-11-12 13:27:03 +0530

    Command Pattern

---
+++ Assignment/src/main/java/CommandPattern/Receiver.java
@@ -0,0 +1,19 @@
+package CommandPattern;
+
+class Light{
+    void turnOn(){
+        System.out.println("Light Is ON!");
+    }
+    void turnOff() {
+        System.out.println("Light is OFF");
+    }
+}
+
+class Fan{
+    void turnOn(){
+        System.out.println("Fan Is ON!");
+    }
+    void turnOff() {
+        System.out.println("Fan is OFF");
+    }
+}
